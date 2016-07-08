db3fdbcb48eef7f5fa544f84a3ba5d7bd21704c0
zhongsp <patrick.zhongsp@gmail.com>
2016-07-08 11:20:59 +0800

    pretty code

--- app/scripts/controllers/ChartSetController.js
+++ app/scripts/controllers/ChartSetController.js
@@ -11,20 +11,23 @@ angular.module('eagleeye')
   .controller('ChartSetController', [
     '$stateParams',
     'EagleEyeWebService',
-    function ($stateParams, EagleEyeWebService) {
+    function($stateParams, EagleEyeWebService) {
       var controller = this,
-      id = $stateParams.id;
-      controller.charDataArray = new Array();
+        id = $stateParams.id;
+
+      controller.charDataArray = [];
+
       EagleEyeWebService.getChartSetById(id).then(function(settings) {
         controller.settings = settings;
+
         var chartsArray = controller.settings.charts;
         var chartCount = chartsArray.length == undefined ? 0 : chartsArray.length;
-        for(var i = 0 ; i < chartCount ; i++){
+
+        for (var i = 0; i < chartCount; i++) {
           EagleEyeWebService.getChartById(chartsArray[i]).then(function(data) {
             controller.charDataArray.push(data);
           });
         }
-
       });
     }
   ]);
