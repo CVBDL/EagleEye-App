bdfa3f374cbc9ef72837effd9d6dee5a3f308c97
Jory Wenqiang Wang <jwang34@rockwellautomation.com>
2016-07-11 18:24:57 +0800

    Update Save Image..

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -28,6 +28,7 @@ module.exports = function(config) {
       'bower_components/angular-aria/angular-aria.js',
       'bower_components/angular-messages/angular-messages.js',
       'bower_components/angular-material/angular-material.js',
+      'bower_components/jspdf/dist/jspdf.debug.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       'app/scripts/**/*.js',
