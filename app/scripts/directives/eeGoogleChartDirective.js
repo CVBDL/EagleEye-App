15efdf0d69065f54bacf61cd94727efc9e96759b
zhongsp <patrick.zhongsp@gmail.com>
2016-07-12 09:57:34 +0800

    Redraw google charts when browser window is resized

--- app/scripts/directives/eeGoogleChartDirective.js
+++ app/scripts/directives/eeGoogleChartDirective.js
@@ -9,8 +9,9 @@
 angular.module('eagleeye')
   .directive('eeGoogleChart', [
     '$timeout',
+    '$window',
     'GoogleChartsService',
-    function ($timeout, GoogleChartsService) {
+    function($timeout, $window, GoogleChartsService) {
       var defaultChartOptions = GoogleChartsService.getDefaultChartOptions();
 
       return {
@@ -21,33 +22,46 @@ angular.module('eagleeye')
           chartType: '='
         },
         restrict: 'E',
-        link: function postLink(scope, element) {
-          var chartNode = element[0];
+        link: postLinkFn
+      };
 
-          function drawChart() {
-            google.charts.setOnLoadCallback(function drawOnLoad() {
-              if (!scope.chartType || !scope.chartDataTable) {
-                return;
-              }
+      function postLinkFn(scope, element) {
+        var chartNode = element[0],
+          redrawDelayOnResizeInMs = 200,
+          timeoutID = -1;
 
-              var chart = new google.visualization[scope.chartType](chartNode);
-              var chartDataTable = new google.visualization.DataTable(angular.copy(scope.chartDataTable, {}));
-              var chartOptions = angular.extend(scope.chartOptions, defaultChartOptions[scope.chartType.toLowerCase()]);
+        function drawChart() {
+          google.charts.setOnLoadCallback(function drawOnLoad() {
+            if (!scope.chartType || !scope.chartDataTable) {
+              return;
+            }
 
-              $timeout(chart.draw(chartDataTable, chartOptions), 0);
-            });
-          }
+            var chart = new google.visualization[scope.chartType](chartNode);
+            var chartDataTable = new google.visualization.DataTable(angular.copy(scope.chartDataTable, {}));
+            var chartOptions = angular.extend(scope.chartOptions, defaultChartOptions[scope.chartType.toLowerCase()]);
 
-          var unwatchChartType = scope.$watch('chartType', drawChart);
-          var unwatchChartOptions = scope.$watch('chartOptions', drawChart);
-          var unwatchChartDataTable = scope.$watch('chartDataTable', drawChart);
-
-          scope.$on('$destroy', function() {
-            unwatchChartType();
-            unwatchChartOptions();
-            unwatchChartDataTable();
+            $timeout(chart.draw(chartDataTable, chartOptions), 0);
           });
         }
-      };
+
+        function windowResizeHandler() {
+          $window.clearTimeout(timeoutID);
+          timeoutID = $window.setTimeout(function() {
+            drawChart();
+          }, redrawDelayOnResizeInMs);
+        }
+
+        var unwatchChartType = scope.$watch('chartType', drawChart);
+        var unwatchChartOptions = scope.$watch('chartOptions', drawChart);
+        var unwatchChartDataTable = scope.$watch('chartDataTable', drawChart);
+
+        angular.element($window).on('resize', windowResizeHandler);
+
+        scope.$on('$destroy', function() {
+          unwatchChartType();
+          unwatchChartOptions();
+          unwatchChartDataTable();
+        });
+      }
     }
   ]);
