c551887f68805f050474f9fc16b35031fb47eb6d
zhongsp <patrick.zhongsp@gmail.com>
2016-09-07 13:41:32 +0800

    update Gruntfile to include custom templates

--- Gruntfile.js
+++ Gruntfile.js
@@ -376,7 +376,7 @@ module.exports = function (grunt) {
           usemin: 'scripts/scripts.js'
         },
         cwd: '<%= yeoman.app %>',
-        src: 'views/{,*/}*.html',
+        src: ['views/{,*/}*.html', 'scripts/templates/{,*/}*.html'],
         dest: '.tmp/templateCache.js'
       }
     },
