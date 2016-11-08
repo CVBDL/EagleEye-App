a6a6935adb1b78b63f450e3a7cbb3412a218273e
zhongsp <patrick.zhongsp@gmail.com>
2016-11-08 16:10:04 +0800

    unit test: eeFriendlyUrlValidator directive

--- app/scripts/directives/eeFriendlyUrlValidator.js
+++ app/scripts/directives/eeFriendlyUrlValidator.js
@@ -1,53 +1,52 @@
 'use strict';
 
+angular.module('eagleeye')
+
 /**
  * @ngdoc directive
  * @name eagleeye.directive:eeFriendlyUrlValidator
- * @description
- * # eeFriendlyUrlValidator
+ * @requires eagleeye.EagleEyeWebService
  */
-angular.module('eagleeye')
-  .directive('eeFriendlyUrlValidator', [
-    '$http',
-    'EagleEyeWebService',
-    function($http, EagleEyeWebService) {
-      return {
-        restrict: 'A',
-        require: 'ngModel',
-        link: function eeFriendlyUrlValidatorPostLinkFn($scope, $element, $attrs, ngModel) {
-          var type = $attrs.eeFriendlyUrlValidator,
-            params = {
-              'chart': {
-                method: 'getChartById',
-                prefix: 'c-'
-              },
-              'chartset': {
-                method: 'getChartSetById',
-                prefix: 's-'
-              }
-            };
+.directive('eeFriendlyUrlValidator', [
+  'EagleEyeWebService',
+  function(EagleEyeWebService) {
+    return {
+      restrict: 'A',
+      require: 'ngModel',
+      link: function eeFriendlyUrlValidatorPostLinkFn($scope, $element, $attrs, ngModel) {
+        var type = $attrs.eeFriendlyUrlValidator,
+          params = {
+            'chart': {
+              method: 'getChartById',
+              prefix: 'c-'
+            },
+            'chartset': {
+              method: 'getChartSetById',
+              prefix: 's-'
+            }
+          };
 
-          ngModel.$parsers.push(validator);
+        ngModel.$parsers.push(validator);
 
-          function validator(friendlyUrl) {
-            if (!friendlyUrl || friendlyUrl.length == 0) return;
+        function validator(friendlyUrl) {
+          if (!friendlyUrl || friendlyUrl.length == 0) return;
 
-            ngModel.$setValidity('friendlyUrlChecking', false);
-            ngModel.$setValidity('friendlyUrlAvailable', true);
+          ngModel.$setValidity('friendlyUrlChecking', false);
+          ngModel.$setValidity('friendlyUrlAvailable', true);
 
-            EagleEyeWebService[params[type].method](params[type].prefix + friendlyUrl)
-              .then(function(data) {
-                ngModel.$setValidity('friendlyUrlChecking', true);
-                ngModel.$setValidity('friendlyUrlAvailable', false);
+          EagleEyeWebService[params[type].method](params[type].prefix + friendlyUrl)
+            .then(function(data) {
+              ngModel.$setValidity('friendlyUrlChecking', true);
+              ngModel.$setValidity('friendlyUrlAvailable', false);
 
-              }, function(error) {
-                ngModel.$setValidity('friendlyUrlChecking', true);
-                ngModel.$setValidity('friendlyUrlAvailable', true);
-              });
+            }, function(error) {
+              ngModel.$setValidity('friendlyUrlChecking', true);
+              ngModel.$setValidity('friendlyUrlAvailable', true);
+            });
 
-            return friendlyUrl;
-          }
+          return friendlyUrl;
         }
-      };
-    }
-  ]);
+      }
+    };
+  }
+]);
--- test/spec/directives/eeFriendlyUrlValidator.spec.js
+++ test/spec/directives/eeFriendlyUrlValidator.spec.js
@@ -1,20 +1,170 @@
 'use strict';
 
 describe('Directive: eeFriendlyUrlValidator', function() {
+  var $compile,
+    $rootScope,
+    $httpBackend,
+    ngModel;
 
-  // load the directive's module
+  var $scope,
+    getChartRequestHandler,
+    getChartSetRequestHandler;
+
+  // load main module
   beforeEach(module('eagleeye'));
 
-  var element,
-    scope;
-
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
+  // reset router
+  beforeEach(module(function($urlRouterProvider) {
+    $urlRouterProvider.otherwise(function() { return false; });
   }));
 
-  // it('should make hidden element visible', inject(function ($compile) {
-  //   element = angular.element('<ee-friendly-url-validator></ee-friendly-url-validator>');
-  //   element = $compile(element)(scope);
-  //   expect(element.text()).toBe('this is the friendlyUrlValidator directive');
-  // }));
+  beforeEach(inject(function(_$compile_, _$rootScope_, _$httpBackend_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+    $httpBackend = _$httpBackend_;
+  }));
+
+  beforeEach(function() {
+    var config = {
+      "root_endpoint": "http://127.0.0.1:3000/"
+    };
+
+    $httpBackend.when('GET', '../config.json').respond(config);
+    getChartRequestHandler = $httpBackend.when('GET', 'http://127.0.0.1:3000/api/v1/charts/c-bar-chart').respond('');
+    getChartSetRequestHandler = $httpBackend.when('GET', 'http://127.0.0.1:3000/api/v1/chart-sets/s-chart-set-one').respond('');
+  });
+
+  beforeEach(inject(function($rootScope) {
+    $scope = $rootScope.$new();
+  }));
+
+  afterEach(function() {
+    $httpBackend.verifyNoOutstandingExpectation();
+    $httpBackend.verifyNoOutstandingRequest();
+  });
+
+  function directiveElementFactory(type) {
+    var element;
+
+    element = angular.element(
+      '<form name="form">' +
+        '<input name="friendlyUrl" ee-friendly-url-validator="' + type + '" ng-model="friendlyUrl">' +
+      '</form>'
+    );
+    element = $compile(element)($scope);
+
+    return element;
+  }
+
+  function isFriendlyUrlCheckingValid() {
+    return !!!$scope.form.friendlyUrl.$error.friendlyUrlChecking;
+  }
+
+  function isFriendlyUrlAvailableValid() {
+    return !!!$scope.form.friendlyUrl.$error.friendlyUrlAvailable;
+  }
+
+  describe('chart', function() {
+
+    it('should initialize friendlyUrl validity state', function() {
+      var element = directiveElementFactory('chart');
+
+      expect(isFriendlyUrlCheckingValid()).toBe(true);
+      expect(isFriendlyUrlAvailableValid()).toBe(true);
+    });
+
+    it('should set `friendlyUrlChecking` to false and `friendlyUrlAvailable` to true before checking on server', function() {
+      var element = directiveElementFactory('chart');
+
+      $scope.form.friendlyUrl.$setViewValue('bar-chart');
+      $rootScope.$digest();
+
+      expect(isFriendlyUrlCheckingValid()).toBe(false);
+      expect(isFriendlyUrlAvailableValid()).toBe(true);
+
+      getChartRequestHandler.respond({});
+      $httpBackend.flush();
+
+      expect(isFriendlyUrlCheckingValid()).toBe(true);
+      expect(isFriendlyUrlAvailableValid()).toBe(false);
+    });
+
+    it('should set `friendlyUrlChecking` to true and `friendlyUrlAvailable` to false if friendlyUrl already exist', function() {
+      var element = directiveElementFactory('chart');
+
+      $scope.form.friendlyUrl.$setViewValue('bar-chart');
+      $rootScope.$digest();
+
+      getChartRequestHandler.respond({});
+      $httpBackend.flush();
+
+      expect(isFriendlyUrlCheckingValid()).toBe(true);
+      expect(isFriendlyUrlAvailableValid()).toBe(false);
+    });
+
+    it('should set `friendlyUrlChecking` to true and `friendlyUrlAvailable` to true if friendlyUrl not exist', function() {
+      var element = directiveElementFactory('chart');
+
+      $scope.form.friendlyUrl.$setViewValue('bar-chart');
+      $rootScope.$digest();
+
+      getChartRequestHandler.respond(500);
+      $httpBackend.flush();
+
+      expect(isFriendlyUrlCheckingValid()).toBe(true);
+      expect(isFriendlyUrlAvailableValid()).toBe(true);
+    });
+  });
+
+  describe('chart set', function() {
+
+    it('should initialize friendlyUrl validity state', function() {
+      var element = directiveElementFactory('chartset');
+
+      expect(isFriendlyUrlCheckingValid()).toBe(true);
+      expect(isFriendlyUrlAvailableValid()).toBe(true);
+    });
+
+    it('should set `friendlyUrlChecking` to false and `friendlyUrlAvailable` to true before checking on server', function() {
+      var element = directiveElementFactory('chartset');
+
+      $scope.form.friendlyUrl.$setViewValue('chart-set-one');
+      $rootScope.$digest();
+
+      expect(isFriendlyUrlCheckingValid()).toBe(false);
+      expect(isFriendlyUrlAvailableValid()).toBe(true);
+
+      getChartSetRequestHandler.respond({});
+      $httpBackend.flush();
+
+      expect(isFriendlyUrlCheckingValid()).toBe(true);
+      expect(isFriendlyUrlAvailableValid()).toBe(false);
+    });
+
+    it('should set `friendlyUrlChecking` to true and `friendlyUrlAvailable` to false if friendlyUrl already exist', function() {
+      var element = directiveElementFactory('chartset');
+
+      $scope.form.friendlyUrl.$setViewValue('chart-set-one');
+      $rootScope.$digest();
+
+      getChartSetRequestHandler.respond({});
+      $httpBackend.flush();
+
+      expect(isFriendlyUrlCheckingValid()).toBe(true);
+      expect(isFriendlyUrlAvailableValid()).toBe(false);
+    });
+
+    it('should set `friendlyUrlChecking` to true and `friendlyUrlAvailable` to true if friendlyUrl not exist', function() {
+      var element = directiveElementFactory('chartset');
+
+      $scope.form.friendlyUrl.$setViewValue('chart-set-one');
+      $rootScope.$digest();
+
+      getChartSetRequestHandler.respond(500);
+      $httpBackend.flush();
+
+      expect(isFriendlyUrlCheckingValid()).toBe(true);
+      expect(isFriendlyUrlAvailableValid()).toBe(true);
+    });
+  });
 });
