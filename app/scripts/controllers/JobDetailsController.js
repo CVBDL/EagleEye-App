808e55fd88aa7dbbc846f55bab2348a3d211eaeb
zhongsp <patrick.zhongsp@gmail.com>
2016-11-26 15:38:29 +0800

    refactor job related controller

--- app/scripts/controllers/JobDetailsController.js
+++ app/scripts/controllers/JobDetailsController.js
@@ -10,15 +10,14 @@ angular.module('eagleeye')
     'EagleEyeWebService',
     function($state, EagleEyeWebService) {
       this.job = {
-        enabled: true
+        enabled: true,
+        customExpression: {}
       };
 
-      this.customExpression = {};
-
-      this.expressionOptions = [/*{
+      this.expressionOptions = [{
         label: 'Use Custom Expression',
         value: 'custom'
-      }, */{
+      }, {
         label: 'Hourly',
         value: '0 * * * *'
       }, {
@@ -35,8 +34,57 @@ angular.module('eagleeye')
         value: '0 0 1 1 *'
       }];
 
+      /**
+       * @method
+       * @name getExpression
+       * @description Calculate the cron expression of the scheduled job.
+       * @params {Object} job
+       * @returns {string} Final job expression.
+       */
+      this.getExpression = function(job) {
+        if (job.expression !== 'custom') {
+          return job.expression;
+
+        } else {
+          return [
+            job.customExpression.minute,
+            job.customExpression.hour,
+            job.customExpression.dayOfMonth,
+            job.customExpression.month,
+            job.customExpression.dayOfWeek
+          ].join(' ');
+        }
+      };
+
+      /**
+       * @method
+       * @name makeJobPayload
+       * @description Generate payload send to server.
+       * @params {Object} job
+       * @returns {Object} Payload object.
+       */
+      this.makeJobPayload = function(job) {
+        var payload = {};
+
+        payload.name = job.name;
+        payload.enabled = job.enabled;
+        payload.command = job.command;
+        payload.expression = this.getExpression(job);
+
+        return payload;
+      };
+
+      /**
+       * @method
+       * @name save
+       * @description Send job payload to server.
+       * @params {Object} job
+       * @returns {promise}
+       */
       this.save = function(job) {
-        return EagleEyeWebService.createJob(job).then(function() {
+        var payload = this.makeJobPayload(job);
+
+        return EagleEyeWebService.createJob(payload).then(function() {
           $state.go('jobs');
         });
       };
--- app/scripts/controllers/JobsController.js
+++ app/scripts/controllers/JobsController.js
@@ -13,11 +13,19 @@ angular.module('eagleeye')
       var controller = this;
 
       this.jobs = [];
-      this.isLoading = true;
       this.totalJobs = 0;
       this.failureJobs = 0;
       this.lastUpdatedAt = null;
 
+      /** @default true */
+      this.isLoading = true;
+
+      /**
+       * @method
+       * @name loadJobs
+       * @description Fetch jobs from server and initialize data model.
+       * @returns {promise}
+       */
       this.loadJobs = function() {
         controller.isLoading = true;
 
@@ -31,28 +39,63 @@ angular.module('eagleeye')
         });
       };
 
+      /**
+       * @method
+       * @name deleteJob
+       * @description Delete specific job from server.
+       * @params {string} jobId The `_id` property of a job.
+       * @returns {promise}
+       */
       this.deleteJob = function(jobId) {
         return EagleEyeWebService.deleteJob(jobId).then(function() {
           controller.loadJobs();
         });
       };
 
+      /**
+       * @method
+       * @name restartJob
+       * @description Restart a job immediately.
+       * @params {string} jobId The `_id` property of a job.
+       * @returns {promise}
+       */
       this.restartJob = function(jobId) {
         return EagleEyeWebService.restartJob(jobId);
       };
 
+      /**
+       * @method
+       * @name createJob
+       * @description Route to job creation view.
+       */
       this.createJob = function() {
         $state.go('jobCreation');
       };
 
+      /**
+       * @method
+       * @name restartJob
+       * @description Calculate total jobs count.
+       * @params {Object} jobs The set of jobs.
+       */
       this.calculateTotalJobs = function(jobs) {
         this.totalJobs = jobs.length;
       };
 
+      /**
+       * @method
+       * @name restartJob
+       * @description Calculate jobs count with `lastState` is 'failure'.
+       * @params {Object} jobs The set of jobs
+       */
       this.calculateFailureJobs = function(jobs) {
         this.failureJobs = $filter('filter')(jobs, { lastState: 'failure' }).length;
       };
 
+      /**
+       * @method
+       * @name init
+       */
       this.init = function() {
         this.loadJobs();
       };
